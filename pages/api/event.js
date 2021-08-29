abcd886a9dd75bb4d571577b766997e20e546642
Alvin Chang <solstory@gmail.com>
2021-08-28 21:51:51 -0500

    remove event mocks

--- pages/api/event.js
+++ pages/api/event.js
@@ -1,27 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import prisma from '../../lib/prisma';
 
-const mockEvents = {
-  dateUpdated: Date.now(),
-  events: [
-    {
-      name: 'test1',
-      date: Date.now(),
-      description: 'test2',
-    },
-    {
-      name: 'test2',
-      date: Date.now(),
-      description: 'test2',
-    },
-    {
-      name: 'test3',
-      date: Date.now(),
-      description: 'test2',
-    },
-  ],
-};
-
 const createEvent = async (name, date) => prisma.event.create({
   data: {
     name,
