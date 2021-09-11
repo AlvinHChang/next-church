f5e6e755e0a990e60aed12e760dcd3fe32f8284a
Alvin Chang <solstory@gmail.com>
2021-09-11 13:44:08 -0500

    update biography page

--- components/containers/PageContainer.jsx
+++ components/containers/PageContainer.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types';
 import styles from './PageContainer.module.css';
 
-function PageContainer({ children }) {
+function PageContainer({ children, topBottomPadding }) {
   return (
-    <div className={styles.pageContainer}>
-      {children}
+    <div className={topBottomPadding && styles.padding}>
+      <div className={styles.pageContainer}>
+        {children}
+      </div>
     </div>
   );
 }
 
 PageContainer.propTypes = {
   children: PropTypes.node.isRequired,
+  topBottomPadding: PropTypes.bool,
+};
+
+PageContainer.defaultProps = {
+  topBottomPadding: false,
 };
 
 export default PageContainer;
